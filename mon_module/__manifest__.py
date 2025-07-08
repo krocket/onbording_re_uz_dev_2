649fc72e546c69dd9decfb3e7ec759e6356d4fc4
Adrienfdupont <adrienf.dupont@gmail.com>
2025-07-08 15:02:43 +0200

    category relation

--- mon_module/__manifest__.py
+++ mon_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Mon Module",
-    'version': '18.0.2.5',
+    'version': '18.0.2.6',
     'depends': ['base'],
     'author': "Adrienfdupont",
     'category': 'General',
--- mon_module/models/article.py
+++ mon_module/models/article.py
@@ -8,4 +8,4 @@ class Article(models.Model):
     suppliers_ids = fields.Many2many('res.partner', string='Suppliers', required=True)
     description = fields.Text(string='Description')
     active = fields.Boolean(string='Active', default=True)
-    category_ids = fields.Many2one(comodel_name='article.category', string='Categories')
+    category_ids = fields.Many2many(comodel_name='article.category', string='Categories')
