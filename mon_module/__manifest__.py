d59946c13c0c0661b5a5ab95bbfdd3f160f79fbf
Adrienfdupont <adrienf.dupont@gmail.com>
2025-07-08 11:27:28 +0200

    fix module

--- mon_module/__manifest__.py
+++ mon_module/__manifest__.py
@@ -1,8 +1,9 @@
 {
     'name': "Mon Module",
-    'version': '18.1.0',
+    'version': '18.0.1.0',
     'depends': ['base'],
     'author': "Adrienfdupont",
     'category': 'General',
     'description': "Un module de gestion d'articles",
+    'installable': True,
 }
