9321d12cc787c6961996423639458e4030693136
Adrienfdupont <adrienf.dupont@gmail.com>
2025-07-09 15:32:54 +0200

    on status change, set priority

--- re-uz_crm/models/lead.py
+++ re-uz_crm/models/lead.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Lead(models.Model):
     _inherit = 'crm.lead'
@@ -15,3 +15,10 @@ class Lead(models.Model):
         default='1',
         help="Priority of the lead, from low to urgent."
     )
+
+    @api.onchange('stage_id')
+    def _onchange_stage_id(self):
+        if self.stage_id == 2:
+            self.priority = '2'
+        elif self.stage_id == 3:
+            self.priority = '3'
