92e52f0392d0a8133fc9dcf61df47168044ae659
Adrienfdupont <adrienf.dupont@gmail.com>
2025-07-09 15:42:17 +0200

    fix priority change

--- re-uz_crm/models/lead.py
+++ re-uz_crm/models/lead.py
@@ -18,7 +18,10 @@ class Lead(models.Model):
 
     @api.onchange('stage_id')
     def _onchange_stage_id(self):
-        if self.stage_id == 2:
+        stage2 = self.env.ref('crm.crm_stage_lead_2')
+        stage3 = self.env.ref('crm.crm_stage_lead_3')
+        
+        if self.stage_id.id == stage2.id:
             self.priority = '2'
-        elif self.stage_id == 3:
+        elif self.stage_id.id == stage3.id:
             self.priority = '3'
